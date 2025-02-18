e82d187d550e3a24995a813e4d0d8c9a28250126
zelazna <constantin.guidon@gmail.com>
2025-02-18 16:52:13 +0100

    [DOCKER] Setup

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    host: true,
+    watch: {
+      usePolling: true,
+    },
+  },
 })
+
+
