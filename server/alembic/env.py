1b989cd4d0a387717b570f89acee879db573812f
Constantin Guidon <constantin.guidon@gmail.com>
2025-05-04 19:59:01 +0200

    [SERVER] Add initial models and update Alembic environment

--- server/alembic/env.py
+++ server/alembic/env.py
@@ -2,8 +2,9 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
 from alembic import context
+
+from models import SQLModel  # noqa
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
