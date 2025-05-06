47e7573e15dee7683c9f292c0ca0a5dd9125b846
Constantin Guidon <constantin.guidon@gmail.com>
2025-05-06 07:33:15 +0200

    [SERVER] Add CORS configuration and initial user setup in environment and database
    - Updated .env file to include BACKEND_CORS_ORIGINS, FIRST_USER, and FIRST_USER_PASSWORD.
    - Enhanced config.py to parse CORS origins and added fields for FIRST_USER and FIRST_USER_PASSWORD.
    - Modified db.py to create the first user if it doesn't exist during database initialization.
    - Changed main.py to use dynamic CORS origins from settings.

--- server/core/config.py
+++ server/core/config.py
@@ -1,15 +1,31 @@
 import secrets
+from typing import Annotated, Any
 
 from pydantic import (
+    AnyUrl,
+    BeforeValidator,
     PostgresDsn,
     computed_field,
 )
 from pydantic_settings import BaseSettings
 
 
+def parse_cors(v: Any) -> list[str] | str:
+    if isinstance(v, str) and not v.startswith("["):
+        return [i.strip() for i in v.split(",")]
+    elif isinstance(v, list | str):
+        return v
+    raise ValueError(v)
+
+
 class Settings(BaseSettings):
-    # REDIS_DSN: RedisDsn
     SECRET_KEY: str = secrets.token_urlsafe(32)
+    BACKEND_CORS_ORIGINS: Annotated[list[AnyUrl] | str, BeforeValidator(parse_cors)] = (
+        []
+    )
+    FIRST_USER: str
+    FIRST_USER_PASSWORD: str
+
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
     API_V1_STR: str = "/api/v1"
@@ -18,6 +34,7 @@ class Settings(BaseSettings):
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str = ""
     POSTGRES_DB: str = ""
+    FRONTEND_HOST: str = "http://localhost:8080"
 
     @computed_field  # type: ignore[prop-decorator]
     @property
@@ -30,6 +47,13 @@ class Settings(BaseSettings):
             port=self.POSTGRES_PORT,
             path=self.POSTGRES_DB,
         )
+
+    @computed_field  # type: ignore[prop-decorator]
+    @property
+    def all_cors_origins(self) -> list[str]:
+        return [str(origin).rstrip("/") for origin in self.BACKEND_CORS_ORIGINS] + [
+            self.FRONTEND_HOST
+        ]
 
 
 settings = Settings()  # type: ignore
--- server/core/db.py
+++ server/core/db.py
@@ -1,6 +1,8 @@
-from sqlmodel import Session, create_engine
-
 from core.config import settings
+from sqlmodel import Session, create_engine, select
+
+from crud import UserCreate, create_user
+from models import User
 
 engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
 
@@ -14,4 +16,13 @@ def init_db(session: Session) -> None:
     # Tables should be created with Alembic migrations
     # This works because the models are already imported and registered from models
     # SQLModel.metadata.create_all(engine)
-    ...
+    user = session.exec(
+        select(User).where(User.username == settings.FIRST_USER)
+    ).first()
+    if not user:
+        user_in = UserCreate(
+            email="first.user@gmail.com",
+            username=settings.FIRST_USER,
+            password=settings.FIRST_USER_PASSWORD,
+        )
+        user = create_user(session=session, user_create=user_in)
